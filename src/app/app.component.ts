43b82083d7c48a69e6af2208ffdc089089038b2b
Sub2n <95su1208@gmail.com>
2019-07-05 16:26:13 +0900

    Nav button add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,22 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <nav>
+    <nav *ngIf="showNav">
       <a routerLink="/subin">Subin</a>
       <a routerLink="/hyunjin">Hyunjin</a>
       <a routerLink="/sooyoung">Sooyoung</a>
     </nav>
+    <button (click)="toggleNav()">Nav 숨기기</button>
     <router-outlet></router-outlet>
   `,
   styles: [
     `
+      :host {
+        position: relative;
+        width: 100vw;
+        display: block;
+        height: 100vh;
+      }
       a {
         margin: 0 30px;
+      }
+      button {
+        position: absolute;
+        top: 0;
+        right: 0;
       }
     `,
   ],
 })
 export class AppComponent {
-  title = 'Signup-Test';
+  showNav: boolean;
+  constructor() {
+    this.showNav = true;
+  }
+  toggleNav() {
+    this.showNav = !this.showNav;
+  }
 }
