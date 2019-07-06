b4541224ad98e5108544db08990525601d4fc32c
Sub2n <95su1208@gmail.com>
2019-07-06 15:50:33 +0900

    subin sign up using Angular Reactive Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HyunjinComponent } from './hyunjin/hyunjin.component';
 import { SooyoungComponent } from './sooyoung/sooyoung.component';
 import { SubinComponent } from './subin/subin.component';
+import { SignupHeaderComponent } from './subin/signup-header/signup-header.component';
+import { SignUpComponent } from './subin/sign-up/sign-up.component';
+import { SignInComponent } from './subin/sign-in/sign-in.component';
+import { SignupFooterComponent } from './subin/signup-footer/signup-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HyunjinComponent,
     SooyoungComponent,
-    SubinComponent
+    SubinComponent,
+    SignupHeaderComponent,
+    SignUpComponent,
+    SignInComponent,
+    SignupFooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
